42253ffde7e3ac2a5fe404a79aaeec1698999a54
June Domingo <junedomingo@gmail.com>
2023-10-28 21:07:25 +0800

    Better approach on tree

--- src/ProvidersTree.tsx
+++ src/ProvidersTree.tsx
@@ -1,31 +1,35 @@
-import React from 'react';
-import { TamaguiProvider, TamaguiProviderProps } from 'tamagui';
+import React, { FC, ReactElement, ReactNode } from 'react';
+import { TamaguiProvider } from 'tamagui';
 import tamaguiConfig from '../tamagui.config';
 
-type Providers = [React.ElementType, React.ComponentProps<any>];
-type Children = {
-  children: React.ElementType[];
+type ProviderProps = {
+  children: ReactNode;
 };
 
-const buildProvidersTree = (componentsWithProps: Providers[]) => {
-  const initialComponent = ({ children }: Children) => <>{children}</>;
+/**
+ * Recursively build a provider tree based on the array of providers.
+ * This starts with an initial component and wraps it in each provider in the array.
+ *
+ * @param providers List of provider components to wrap.
+ */
+const buildProvidersTree = (providers: ReactElement[]): FC<ProviderProps> => {
+  const BaseComponent: FC<ProviderProps> = ({ children }) => <>{children}</>;
 
-  return componentsWithProps.reduce(
-    (
-      AccumulatedComponents: React.ElementType,
-      [Provider, props = {}]: Providers,
-    ) =>
-      ({ children }: Children) => (
-        <AccumulatedComponents>
-          <Provider {...props}>{children}</Provider>
-        </AccumulatedComponents>
-      ),
-    initialComponent,
+  return providers.reduce<FC<ProviderProps>>(
+    (AccumulatedComponent, ProviderComponent) => {
+      return ({ children }) => (
+        <AccumulatedComponent>
+          {React.cloneElement(ProviderComponent, {}, children)}
+        </AccumulatedComponent>
+      );
+    },
+    BaseComponent,
   );
 };
 
+// Create the tree of providers using an array of provider components.
 const ProvidersTree = buildProvidersTree([
-  [TamaguiProvider, { config: tamaguiConfig } as TamaguiProviderProps],
+  <TamaguiProvider config={tamaguiConfig} />,
 ]);
 
 export default ProvidersTree;
