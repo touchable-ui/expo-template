f49cb78fa5e37744e598d9a25db9f985802dd482
June Domingo <junedomingo@gmail.com>
2023-10-28 22:57:32 +0800

    Remove ProvidersTree, setup fonts

--- App.tsx
+++ App.tsx
@@ -1,16 +1,39 @@
 import { useStore } from '@hooks';
 import { initialize as initLocalization } from '@localization';
 import { Navigation } from '@navigation';
-import ProvidersTree from './src/ProvidersTree';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { Stack, TamaguiProvider } from 'tamagui';
+import tamaguiConfig from './tamagui.config';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const language = useStore(state => state.appConfig.language);
+  const [fontsLoaded, fontError] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  const { theme, language } = useStore(state => state.appConfig);
 
   initLocalization(language);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <ProvidersTree>
-      <Navigation />
-    </ProvidersTree>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={theme}>
+      <Stack onLayout={onLayoutRootView} flex={1}>
+        <Navigation />
+      </Stack>
+    </TamaguiProvider>
   );
 }
