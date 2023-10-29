ced3071a948048a99c8b1fb3daefa8e642943819
June Domingo <junedomingo@gmail.com>
2023-10-29 12:58:38 +0800

    Add KeysContain type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,18 @@ export interface Product {
   id: number;
   name: string;
 }
+
+/**
+ * Extracts keys from a type that contain a specific string.
+ * @template T - The type to extract keys from.
+ * @template S - The string to search for in the keys.
+ */
+export type KeysContain<T, S extends string> = Pick<
+  T,
+  /**
+   * Filters the keys of the type T to only include keys that contain the string S.
+   */
+  {
+    [K in keyof T]: K extends `${string}${S}${string}` ? K : never;
+  }[keyof T]
+>;
