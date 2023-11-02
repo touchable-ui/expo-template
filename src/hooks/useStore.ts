e764ec67ddbc2e3d8a1766a91e4e6d9372875f93
June Domingo <junedomingo@gmail.com>
2023-11-02 12:08:33 +0800

    Update merge and add migrate example

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -12,6 +12,34 @@ export const useStore = create<StoreState>()(
     {
       name: 'store',
       storage: createJSONStorage(() => AsyncStorage),
+      merge: (
+        persistedState: unknown,
+        currentState: StoreState,
+      ): StoreState => {
+        if (typeof persistedState === 'object' && persistedState !== null) {
+          return {
+            ...currentState,
+            ...(persistedState as Partial<StoreState>),
+          };
+        }
+
+        return currentState;
+      },
+      // Example of migration, change the default theme from light to dark
+      // migrate: (persistedState: unknown, version: number): StoreState => {
+      //   if (typeof persistedState === 'object' && persistedState !== null) {
+      //     const state = persistedState as StoreState;
+      //     if (version === 0 && state.appConfig.theme === 'light') {
+      //       return {
+      //         ...state,
+      //         appConfig: { ...state.appConfig, theme: 'dark' },
+      //       };
+      //     }
+      //   }
+
+      //   return persistedState as StoreState;
+      // },
+      // version: 0,
     },
   ),
 );
